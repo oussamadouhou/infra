fbd51b57116946c756286a86cbafa070e8ffdabb
Oussama Douhou <oussama.douhou@gmail.com>
2025-11-11 15:08:32 +0100

    Netbird update script

---
+++ networking/netbird/update-netbird.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -euo pipefail
+
+source "$(dirname "$0")/../utils/log.sh"
+source "$(dirname "$0")/../utils/check_root.sh"
+
+log "=== [$(date)] Start NetBird update ==="
+
+curl -fsSL https://pkgs.netbird.io/install.sh | bash
+systemctl restart netbird
+
+if systemctl is-active --quiet netbird; then
+	log "✅ NetBird has been started."
+else
+	log "❌ NetBird did not start. Please check logs."
+	exit
+fi
+
+log "=== Update completed ==="
